b083ba4c684b23f179e240bcb545b1368ce99693
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-07 11:02:33 +0530

    auto complete ui updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-const Navbar = () => {
+function Navbar() {
+
+  const [isAutoCompleteVisible,setIsAutoCompleteVisible] = useState(false);
+
   return (
     <nav className="nav">
-       <div className="nav-brand">
-        <div className="nav-title">Movie Quest</div>
-       </div>
+       <div className="nav-brand">Movie Quest</div>
        <div className="nav-search">
-        <input className="search" type="search" name="search" id="search" placeholder='What movie are you thinking about...'/>
-        <button className="search-btn">Search</button>
+          <input className="search" type="search" name="search" id="search" placeholder='What movie are you thinking about...'
+            onFocus={() => setIsAutoCompleteVisible(true)}
+            onBlur={() => setIsAutoCompleteVisible(false)}
+          />
+          <div className="search-list" style={{display: (isAutoCompleteVisible) ? 'block' : 'none'}}>
+            <div className='auto-complete'>result</div>
+            <div className='auto-complete'>result</div>
+            <div className='auto-complete'>result</div>
+            <div className='auto-complete'>result</div>
+            <div className='auto-complete'>result</div>
+          </div>
        </div>
-       <div className="nav-theme-btn">
-        <button className="theme-btn">Theme</button>
-       </div>
+        <button className="nav-theme-btn">Theme</button>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
