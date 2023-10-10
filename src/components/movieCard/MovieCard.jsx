fd510ad4e557b2e68c1dd171c3c459c684134d04
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-10-10 20:40:06 +0530

    added blur loading img effect

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import './movieCard.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import ReactSvg from '../../assets/react.svg'
 function MovieCard({ Title, Year, Type, Poster,id }) {
     const navigator = useNavigate();
@@ -16,10 +17,11 @@ function MovieCard({ Title, Year, Type, Poster,id }) {
                     className='img' 
                     src={Poster} 
                     placeholderSrc={ReactSvg}
+                    effect="blur"
                 />
             </div>
             <div className='movie-title'>
-                <span>{Title}</span>
+                <span>{Title.length > 17 ? `${Title.substring(0, 17)}...` : Title}</span>
             </div>
             <div className='movie-year'>
                 <span>Released in: {Year}</span>
